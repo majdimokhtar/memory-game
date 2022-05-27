987cd8a75014040cc44a003e2a820cf9c7e126ab
unknown <majdi.mokhtar@gmail.com>
2022-05-27 16:45:42 +0100

    memory game

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import './App.css'
+import {useState,useEffect} from "react"
+import Card from './components/Card'
+
+const cardsImages =[
+  {"src":"/img/helmet-1.png",matched:false },
+  {"src":"/img/potion-1.png",matched:false},
+  {"src":"/img/ring-1.png" ,matched:false},
+  {"src":"/img/scroll-1.png" ,matched:false},
+  {"src":"/img/shield-1.png" ,matched:false},
+  {"src":"/img/sword-1.png",matched:false }
+]
+
+function App() {
+  const [cards,setCards] = useState([])
+  const [turns ,setTurns] =useState(0)
+  const [choiceOne ,setChoiceOne] = useState(null)
+  const [choiceTwo ,setChoiceTwo] = useState(null)
+  const [disabled ,setDisabled] = useState(false)
+  function playSound () {
+    let ding = new Audio('/win.wav');
+    ding.play();
+  }
+
+  //duplicate each cards once
+  const duplicateCards = ()=>{
+   const duplicatedCards=[...cardsImages , ...cardsImages]
+   .sort(()=> Math.random() -0.5)
+   .map((card)=>({...card, id : Math.random()}))
+   setChoiceOne(null)
+   setChoiceTwo(null)
+   setCards(duplicatedCards)
+   setTurns(0)
+  }
+  //comparing two cards
+  useEffect(() => {
+    
+    if (choiceOne&&choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src===choiceTwo.src) {
+        setCards(prevCards =>{
+          return prevCards.map(card=>{
+            if (card.src===choiceOne.src) {
+              return {...card,matched:true}
+            } else{
+              return card
+            }
+          })
+        })
+        playSound()
+        resetTurn()
+      }
+      else{
+        setTimeout(() => resetTurn(), 1000);
+        
+      }
+      
+    }
+    
+  }, [choiceOne,choiceTwo])
+  console.log(cards);
+  //reset choices
+  const resetTurn = () =>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns +1)
+    setDisabled(false)
+  }
+  // start automattically
+  useEffect(()=>{
+    duplicateCards()
+  },[])
+
+  // handlechoice
+  const handleChoice =(card)=>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    console.log(card);
+  }
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={duplicateCards}>New Game</button>
+      <div className='cards-grid'>
+      {cards.map((card)=> (<Card card={card}
+       key={card.id}
+       handleChoice={handleChoice} 
+       flipped ={card===choiceOne || card ===choiceTwo || card.matched}
+       disabled={disabled}
+       /> ))}
+         </div>
+      <p>Turns : {turns} </p>
+    </div>
+  );
+}
+
+export default App
